c4e5f381124b18db67b70f634379be25a4be6ee7
JESSTADA <jesstada@gmail.com>
2019-10-08 09:46:17 -0700

    update contract to make fewer calls by saving contract data to variable

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { EthereumEvent, BigInt, Bytes,  } from "@graphprotocol/graph-ts"
+import { EthereumEvent, BigInt, Bytes  } from "@graphprotocol/graph-ts"
 import {
   Contract,
   PaymentExecuted,
@@ -10,14 +10,15 @@ import { log } from '@graphprotocol/graph-ts'
 
 export function handlePaymentExecuted(event: PaymentExecuted): void {
   let contract = Contract.bind(event.address)
+  let authPaymentData = contract.authorizedPayments(event.params.idPayment)
   let entity = Payment.load(event.params.idPayment.toString())
   if(entity == null){
     entity = new Payment(event.params.idPayment.toString()) 
-    entity.name = contract.authorizedPayments(event.params.idPayment).value0
-    entity.spender = contract.authorizedPayments(event.params.idPayment).value2
-    entity.recipient = contract.authorizedPayments(event.params.idPayment).value6
-    entity.token = contract.authorizedPayments(event.params.idPayment).value7
-    entity.amount = contract.authorizedPayments(event.params.idPayment).value8
+    entity.name = authPaymentData.value0
+    entity.spender = authPaymentData.value2
+    entity.recipient = authPaymentData.value6
+    entity.token = authPaymentData.value7
+    entity.amount = authPaymentData.value8
   }
   entity.status = "executed"
   entity.save()
@@ -38,30 +39,42 @@ export function handlePaymentExecuted(event: PaymentExecuted): void {
 
 export function handlePaymentAuthorized(event: PaymentAuthorized): void {
   let contract = Contract.bind(event.address)
+  let authPaymentData = contract.authorizedPayments(event.params.idPayment)
   let entity = Payment.load(event.params.idPayment.toString())
   if(entity == null){
     entity = new Payment(event.params.idPayment.toString()) 
-    entity.name = contract.authorizedPayments(event.params.idPayment).value0
-    entity.spender = contract.authorizedPayments(event.params.idPayment).value2
-    entity.recipient = contract.authorizedPayments(event.params.idPayment).value6
-    entity.token = contract.authorizedPayments(event.params.idPayment).value7
-    entity.amount = contract.authorizedPayments(event.params.idPayment).value8
+    entity.name = authPaymentData.value0
+    entity.spender = authPaymentData.value2
+    entity.recipient = authPaymentData.value6
+    entity.token = authPaymentData.value7
+    entity.amount = authPaymentData.value8
   }
   entity.status = "authorized"
   entity.save()
+
+  let meta = MetaData.load("")
+  if(meta == null) {
+    meta = new MetaData("")
+    meta.numOfPaymentsAuthorized = BigInt.fromI32(0)
+    meta.numOfPaymentsCancelled = BigInt.fromI32(0)
+    meta.executedPayments = new Array<string>()  
+  }
+  meta.numOfPaymentsAuthorized = meta.numOfPaymentsAuthorized.plus(BigInt.fromI32(1)) as BigInt
+  meta.save()
 }
 
 export function handlePaymentCanceled(event: PaymentCanceled): void {
   let contract = Contract.bind(event.address)
+  let authPaymentData = contract.authorizedPayments(event.params.idPayment)
   let entity = Payment.load(event.params.idPayment.toString())
   if(entity == null){
     entity = new Payment(event.params.idPayment.toString())
   }
-  entity.name = contract.authorizedPayments(event.params.idPayment).value0
-  entity.spender = contract.authorizedPayments(event.params.idPayment).value2
-  entity.recipient = contract.authorizedPayments(event.params.idPayment).value6
-  entity.token = contract.authorizedPayments(event.params.idPayment).value7
-  entity.amount = contract.authorizedPayments(event.params.idPayment).value8
+  entity.name = authPaymentData.value0
+  entity.spender = authPaymentData.value2
+  entity.recipient = authPaymentData.value6
+  entity.token = authPaymentData.value7
+  entity.amount = authPaymentData.value8
   entity.status = "cancelled"
   entity.save()
 
